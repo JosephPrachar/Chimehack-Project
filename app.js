34066f860350e3f920b86d05a953f23224d6973f
Joseph Prachar <josephprachar@comcast.net>
2016-08-27 16:53:34 -0700

    Change port

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.status(200).send('Hello, world!');
+  res.status(200).send('Hello, chimehack3!');
 });
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!');
 });
 
